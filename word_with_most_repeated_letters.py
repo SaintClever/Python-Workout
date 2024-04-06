6719c52f5e9cb16237c648dae0ff212b44d6459f
Saint. Clever <parchment@gmx.com>
2024-04-05 20:45:59 -0400

    Complete word with most repeated letters challenge

--- word_with_most_repeated_letters.py
+++ word_with_most_repeated_letters.py
@@ -1,6 +1,16 @@
 def most_repeating_word(strings):
-    return "".join(set(i) for i in strings)
+    count = 0
+    char, output = "", ""
+
+    for word in strings:
+        for letter in word:
+            if word.count(letter) > count:
+                count = word.count(letter)
+                char = letter
+                output = word
+
+    return f"{output}: with {count} {char.upper()!r}s"
 
 
-words = ["this", "is", "an", "elementary", "test", "example"]
+words = ["this", "is", "racecar racer", "an", "elementary", "test", "example"]
 print(most_repeating_word(words))
