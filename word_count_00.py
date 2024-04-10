5f69e576ff43a4f6ead6c6dcc257f3274b45b26d
Saint. Clever <parchment@gmx.com>
2024-04-10 17:08:37 -0400

    Create dummy text for word count and a second version

---
+++ word_count_00.py
@@ -0,0 +1,45 @@
+from pprint import pprint
+
+
+"""
+The challenge for this exercise is to write a wordcount function that mimics the wc  Unix command. The function will take a filename as input and will print four lines  of output:  1 Number of characters (including whitespace)  2 Number of words (separated by whitespace)  3 Number of lines  4 Number of unique words (case sensitive, so “NO” is different from “no”)
+
+
+This is a test file.  
+
+It contains 28 words and 20 different words.  
+
+It also contains 165 characters.  
+
+It also contains 11 lines.  
+
+It is also self-referential.  
+
+Wow! 
+"""
+
+
+def word_count(file_name):
+    output = {
+        "characters": 0,  # 165
+        "different_words": 0,  # 20
+        "lines": 0,  # 11
+        "new_line": 0,  # 11
+        "whitespace": 0,  # 22
+        "words": 0,  # 28
+    }
+
+    with open(file_name) as f:
+        file = f.read()
+
+    output["characters"] = len(file)
+    output["different_words"] = len(set(file.split()))
+    output["lines"] = len([sentence for sentence in file.strip().split("\n")])
+    output["new_line"] = len([word for word in file if word == "\n"])
+    output["whitespace"] = len([word for word in file if word == " "])
+    output["words"] = len(set(file))
+
+    return output
+
+
+pprint(word_count("wcfile.txt"))
---
+++ word_count_01.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+
+
+"""
+The challenge for this exercise is to write a wordcount function that mimics the wc  Unix command. The function will take a filename as input and will print four lines  of output:  1 Number of characters (including whitespace)  2 Number of words (separated by whitespace)  3 Number of lines  4 Number of unique words (case sensitive, so “NO” is different from “no”)
+
+
+This is a test file.  
+
+It contains 28 words and 20 different words.  
+
+It also contains 165 characters.  
+
+It also contains 11 lines.  
+
+It is also self-referential.  
+
+Wow! 
+"""
+
+
+def wordcount(filename):
+    counts = {"characters": 0, "words": 0, "lines": 0}
+
+    unique_words = set()
+
+    for one_line in open(filename):
+        counts["lines"] += 1
+        counts["characters"] += len(one_line)
+        counts["words"] += len(one_line.split())
+        unique_words.update(one_line.split())
+        counts["unique words"] = len(unique_words)
+
+        for key, value in counts.items():
+            print(f"{key}: {value}")
+
+
+wordcount("wcfile.txt")
