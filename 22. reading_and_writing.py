3ec5bcc13adeece838e9519d2dcd4ed09b4d2acf
Saint. Clever <parchment@gmx.com>
2024-04-19 18:37:52 -0400

    Create input txt, export to csv function

--- 22. reading_and_writing.py
+++ 22. reading_and_writing.py
@@ -1,2 +1,25 @@
-def passwd_to_csv():
-    return
+import csv
+
+
+def passwd_to_csv(input_txt, output_csv):
+    output = [["key", "value"]]
+
+    # Input: TXT
+    with open(input_txt) as f:
+        txt_file = f.readlines()
+
+        for i in txt_file:
+            if ":" in i:
+                key = i[: i.find(":")]
+                value = i[i.find("*:") + 2 :]
+                output.append([key, value[: value.find(":")]])
+
+    # Output: CSV
+    with open(output_csv, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(output)
+
+    return f"\nFile: {output_csv}\n"
+
+
+print(passwd_to_csv("docs/passwd.txt", "docs/file.csv"))
